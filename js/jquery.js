0103a0e86a42a7c84dbd0a1b770d689a58a41b6e
Kevin D. Silva <kevin.dasilva1@gmail.com>
2017-05-30 11:06:41 -0400

    working on fadeIN bug fix so it doesn't remove the elemnts from the DOM... This will be annoying lol

--- js/jquery.js
+++ js/jquery.js
@@ -2,25 +2,24 @@
 $(document).ready(function() {
 
   function fadeName(){
-    $('#header h1').fadeIn(1000, function(){
-      fadeDev();
-    });
+    $('#header h1').css("visibility","visible").css("transition"," 10s");
   }
 
+  fadeName();
   function fadeDev(){
-    $('#header p').fadeIn(1000, function(){
+    $('#header p').hide().fadeIn(1000, function(){
       fadeIcons();
     });
   }
 
   function fadeIcons(){
-    $('#header i').fadeIn(500, function(){
+    $('#header i').hide().fadeIn(500, function(){
       fadeQuote();
     });
   }
 
   function fadeQuote(){
-    $('.first').find('h1, p, i').fadeIn();
+    $('.first').find('h1, p, i').hide().fadeIn(1000);
   }
 
   fadeName();
